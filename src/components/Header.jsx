8e309a7948a1f0f5612adfdc9be6aa9757197062
Harsh10 <harshsahani078@gmail.com>
2025-06-06 11:07:04 +0530

    Header component done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div >
 
             <h1 className=' text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-700 leading-tight'>
-                Remove the <br /> <span className=' bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>background</span> from <br />images for free.
+                Remove the <br className=' max-md:hidden' /> <span className=' bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>background</span> from <br className=' max-md:hidden' />images for free.
             </h1>
 
             <p className=' my-6 text-[15px] text-gray-500'>Say goodbye to complex editing tools. Our AI removes image backgrounds <br /> in seconds — perfect for e-commerce, design, and marketing. </p>
@@ -24,6 +24,9 @@ const Header = () => {
         </div>
 
         {/* Right Side */}
+        <div className=' w-full max-w-md'> 
+            <img src={assets.header_img} alt="" />
+        </div>
     </div>
   )
 }
