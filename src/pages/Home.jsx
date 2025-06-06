1cb4eec6bf168fce61bb4532d16c35761daf27da
Harsh10 <harshsahani078@gmail.com>
2025-06-06 21:46:12 +0530

    Testimonial component done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import BgSlider from '../components/BgSlider'
+import Testimonials from '../components/Testimonials'
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
         <Header/>
         <Steps/>
         <BgSlider/>
+        <Testimonials/>
     </div>
   )
 }
