c05c5d44b7301382a1226b84c95c1e31d5ee7c97
Harsh10 <harshsahani078@gmail.com>
2025-06-06 12:19:37 +0530

    Steps component

---
+++ src/components/Steps.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Steps = () => {
+  return (
+    <div className=' mx-4 lg:mx-44 py-20 xl:py-40'>
+        <h1 className=' text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-bold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>Steps to remove background <br /> image in seconds</h1>
+
+        <div className=' flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center'>
+
+            <div className=' flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:sca transition-all duration-500'>
+                <img className=' max-w-9' src={assets.upload_icon} alt="" />
+                <div className=' flex flex-col'>
+                <p className=' text-xl font-medium'>Upload image</p>
+                <p className=' text-sm text-neutral-500 mt-1'>Drag and drop or select a JPG/PNG file. <br />Your upload is safe and processed securely.</p>
+                </div>
+            </div>
+
+            <div className=' flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:sca transition-all duration-500'>
+                <img className=' max-w-9' src={assets.remove_bg_icon} alt="" />
+                <div className=' flex flex-col'>
+                <p className=' text-xl font-medium'>Remove background</p>
+                <p className=' text-sm text-neutral-500 mt-1'>Sit back and relax! Our AI is cleaning up <br /> your image and removing the background </p>
+                </div>
+            </div>
+
+            <div className=' flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:sca transition-all duration-500'>
+                <img className=' max-w-9' src={assets.download_icon} alt="" />
+                <div className=' flex flex-col'>
+                <p className=' text-xl font-medium'>Download image</p>
+                <p className=' text-sm text-neutral-500 mt-1'>Export your processed image in <br /> high resolution.Optimized for quality </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Steps
